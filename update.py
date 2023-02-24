7886bc71c80747d432571696ba58296a47c05fe1
Wu Tingfeng <wutingfeng@outlook.com>
2023-02-24 17:38:01 +0800

    Improve ip address detection

--- update.py
+++ update.py
@@ -14,13 +14,14 @@ rows: list[tuple[str, str]] = []
 for url in urls:
     # IP addresses in cloud virtual machine hostnames tend to exist
     # in the form of {number}-{number}-{number}-{number}
+    # or the form of {number}.{number}.{number}.{number}
 
     # Only extract IP addresses from subdomain
     r = tldextract.extract(url)
     # Reverse subdomain before regex search
     # to prevent mixed alphanumeric leading segments from being included (e.g. "ec2-")
     match = re.search(
-        "([0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3})", r.subdomain[::-1]
+        "([0-9]{1,3}[-\.][0-9]{1,3}[-\.][0-9]{1,3}[-\.][0-9]{1,3})", r.subdomain[::-1]
     )
     # Ensure extracted IP address is valid
     if match is not None:
